d343681a953f747e73a3b62d8e51f504b126c5a6
Bluebell <40646666+JyggalagSila@users.noreply.github.com>
2021-02-06 20:56:55 +0100

    Nesto malo menjano, manje-vise gotovo.

--- Projekat/index.js
+++ Projekat/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const path = require('path'); 
 
 const logger = require('./middleware/logger');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'static'))); 
 
-// app.use(logger);
+app.use(logger);
+app.engine('pug', require('pug').__express)
+app.set('view engine', 'pug')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/prisoners', require('./routes/prisoners'))
+app.use('/recipes', require('./routes/recipes'))
 
 app.get('/', function (req, res) {
-    res.send('hello world');
+    res.render('index', { title: 'BestRecipesWarld', heading: 'Welcome! Here you will find all the best recipes in the warld.' })
   });
 
 const port = process.env.PORT || 5000;
